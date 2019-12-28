167c766c62cae3fbbbf9181aa9e3a243d02b592b
JasonMuscles <wangjinsenufo@qq.com>
2019-12-29 03:32:17 +0800

    增加模板

--- booktest/urls.py
+++ booktest/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from booktest import views
 
+# url严格匹配开头和结尾
 urlpatterns = [
     # 通过url函数设置url路由配置项
-    url(r'^index', views.index),   # 建立/index和视图index之间的关系
-    url(r'^booktest', views.booktest),
+    url(r'^index$', views.index),   # 建立/index和视图index之间的关系
+    url(r'^booktest$', views.booktest),
 ]
--- booktest/views.py
+++ booktest/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from django.template import loader, RequestContext
+
 # Create your views here.
 # 1.定义一个视图,HttpResponse
 # 2.进行url配置建立地址和视图的对应关系
@@ -7,7 +9,20 @@ from django.http.response import HttpResponse
 
 def index(request):
     # M和T进行交互处理
-    return HttpResponse("老铁，没病")
+    # return HttpResponse("老铁，没病")
+    # 1.加载模板文件（路径相当对于是templates目录）
+    temp = loader.get_template('booktest/index.html')
+
+    # 2.定义一个模板上下文：给模板文件传递数据
+    context = RequestContext(request, {})
+    context.push(locals())
+
+    # 3.模板渲染：产生标准的html的内容
+    # res_html = temp.render(context)
+    res_html = temp.render(context=locals(), request=request)
+
+    # 4.返回给浏览器
+    return HttpResponse(res_html)
 
 
 def booktest(request):
